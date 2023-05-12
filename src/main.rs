b8bb4332b1eb0b2b03f1419675aa3dbad46bb131
Scott-Kinzer <zaz765831@gmail.com>
2023-05-12 19:00:25 +0300

    feat: added port sniffering

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use::std::env;
 use std::process;
 use port_sniffer::Arguments;
+use std::thread;
+use std::sync::mpsc::channel;
+mod scan;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,7 +11,7 @@ fn main() {
 
     program.remove(0);
 
-    let _args = Arguments::new(&args).unwrap_or_else(|err| {
+    let arguments = Arguments::new(&args).unwrap_or_else(|err| {
         if err.contains("help") {
             process::exit(0);
         } else {
@@ -16,4 +19,31 @@ fn main() {
             process::exit(0);
         }
     });
+
+    let num_threads = arguments.threads;
+    let addr = arguments.ipaddr;
+
+    let (tx, rx) = channel();
+
+    for i in 0..num_threads {
+        let tx  = tx.clone();
+        thread::spawn(move || {
+            scan::scan_mod::scan_port(tx, i, addr, num_threads);
+        });
+    }
+
+    let mut out = vec![];
+    drop(tx);
+
+    for p in rx {
+        out.push(p);
+    }
+
+    println!("");
+    out.sort();
+
+    for i in out {
+        println!("{}", i);
+    }
+
 }
